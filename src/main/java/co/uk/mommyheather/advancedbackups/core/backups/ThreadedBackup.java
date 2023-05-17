aa3679fe2140db1835e172c5bedf790aabdc53b6
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-05-17 15:29:22 +0100

    Differentials/incrementals, misc improvements.

--- src/main/java/co/uk/mommyheather/advancedbackups/core/backups/ThreadedBackup.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/core/backups/ThreadedBackup.java
@@ -2,32 +2,72 @@ package co.uk.mommyheather.advancedbackups.core.backups;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.file.SimplePathVisitor;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import co.uk.mommyheather.advancedbackups.PlatformMethodWrapper;
+import co.uk.mommyheather.advancedbackups.core.backups.gson.DifferentialManifest;
 import co.uk.mommyheather.advancedbackups.core.config.AVConfig;
 
 public class ThreadedBackup extends Thread {
+    private static GsonBuilder builder = new GsonBuilder(); 
+    private static Gson gson;
+    private long delay;
+    private static int count;
+    public static boolean running = false;
     
+    static {
+        builder.setPrettyPrinting();
+        gson = builder.create();
+    }
+    
+    public ThreadedBackup(long delay) {
+        setName("AB Active Backup Thread");
+        this.delay = delay;
+        count = 0;
+    }
+
     @Override
     public void run() {
+        try {
+            sleep(delay);
+        } catch (InterruptedException e) {
+            return;
+        }
+        if (running) {
+            return;
+        }
+        running = true;
+
         File file = new File(AVConfig.config.getPath());
 
         switch(AVConfig.config.getBackupType()) {
             case "zip" : {
                 makeZipBackup(file);
                 break;
-            } 
+            }
+            case "differential" : {
+                makeDifferentialOrIncrementalBackup(file, true);
+                break;
+            }
+            case "incremental" : {
+                makeDifferentialOrIncrementalBackup(file, false);
+                break;
+            }
         }
 
         BackupWrapper.finishBackup();
@@ -38,7 +78,8 @@ public class ThreadedBackup extends Thread {
         try {
 
             File zip = new File(file.toString() + "/zips/", serialiseBackupName() + ".zip");
-            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zip));
+            FileOutputStream outputStream = new FileOutputStream(zip);
+            ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
             zipOutputStream.setLevel(AVConfig.config.getCompressionLevel());
 
             Files.walkFileTree(PlatformMethodWrapper.worldDir, new SimplePathVisitor() {
@@ -64,7 +105,7 @@ public class ThreadedBackup extends Thread {
                         return FileVisitResult.CONTINUE;
                 }
             });
-
+            zipOutputStream.flush();
             zipOutputStream.close();
 
         } catch (IOException e){
@@ -72,6 +113,105 @@ public class ThreadedBackup extends Thread {
             e.printStackTrace();
         }
         
+    }
+
+
+    private static void makeDifferentialOrIncrementalBackup(File location, boolean differential) {
+        try {
+            String name = serialiseBackupName();
+            long time = 0;
+            File manifestFile = differential ? new File(location.toString() + "/differential/manifest.json") : new File(location.toString() + "/incremental/manifest.json");
+            DifferentialManifest manifest;
+            if (manifestFile.exists()) {
+                manifest = gson.fromJson(new String(Files.readAllBytes(manifestFile.toPath())), DifferentialManifest.class);
+            }
+            else {
+                manifest = DifferentialManifest.defaultValues();
+            }
+
+            long comp = differential ? manifest.getLastFull() : manifest.getLastPartial();
+            ArrayList<Path> toBackup = new ArrayList<>();
+
+            boolean complete = manifest.getComplete().size() == 0 || manifest.getChain() >= AVConfig.config.getMaxDepth() ? true : false;
+            
+            Files.walkFileTree(PlatformMethodWrapper.worldDir, new SimplePathVisitor() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
+                    Path targetFile;
+                    targetFile = PlatformMethodWrapper.worldDir.relativize(file);
+                    if (targetFile.toFile().getName().compareTo("session.lock") == 0) {
+                        return FileVisitResult.CONTINUE;
+                    }
+                    count++;
+                    if (complete || attributes.lastModifiedTime().toMillis() >= comp) {
+                        toBackup.add(targetFile);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            if (toBackup.size() >= count) {
+                name += "-full";
+            }
+            else {
+                name += complete? "-full":"-partial";
+            }
+
+            
+
+            if (AVConfig.config.getCompressChains()) {
+                File zip = differential ? new File(location.toString() + "/differential/", name +".zip") : new File(location.toString() + "/incremental/", name +".zip");
+                FileOutputStream outputStream = new FileOutputStream(zip);
+                ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
+                zipOutputStream.setLevel(AVConfig.config.getCompressionLevel());
+
+                for (Path path : toBackup) {
+                    zipOutputStream.putNextEntry(new ZipEntry(path.toString()));
+                    byte[] bytes = Files.readAllBytes(new File(PlatformMethodWrapper.worldDir.toString(), path.toString()).toPath());
+                    zipOutputStream.write(bytes, 0, bytes.length);
+                    zipOutputStream.closeEntry();
+                }
+                zipOutputStream.flush();
+                zipOutputStream.close();
+
+                time = zip.lastModified();
+            }
+            else {
+                File dest = differential ? new File(location.toString() + "/differential/", name + "/") :new File(location.toString() + "/incremental/", name + "/");
+                dest.mkdirs();
+                for (Path path : toBackup) {
+                    File out = new File(dest, path.toString());
+                    if (!out.getParentFile().exists()) {
+                        out.getParentFile().mkdirs();
+                    }
+                    Files.copy(new File(PlatformMethodWrapper.worldDir.toString(), path.toString()).toPath(), out.toPath());
+                }
+                time = dest.lastModified();
+            }
+
+            //Finally, update + write the manifest
+            if (complete || toBackup.size() >= count) {
+                manifest.setChain(0);
+                manifest.getComplete().add(time);
+                manifest.setLastFull(time);
+            }
+            else {
+                manifest.setChain(manifest.getChain() + 1);
+                manifest.getPartial().add(new Date().getTime());
+                manifest.setLastPartial(new Date().getTime());
+            }
+
+            FileWriter writer = new FileWriter(manifestFile);
+            writer.write(gson.toJson(manifest));
+            writer.flush();
+            writer.close();
+    
+        } catch (IOException e) {
+            // TODO Scream at user
+            e.printStackTrace();
+        }
+
+
     }
 
 
