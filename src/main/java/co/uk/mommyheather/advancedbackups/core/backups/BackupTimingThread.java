54c36f67086dcfe901a51a20461835f44fa56bc8
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-05-17 15:27:06 +0100

    Adjustments to the timing mechanism, greatly improves stability.

--- src/main/java/co/uk/mommyheather/advancedbackups/core/backups/BackupTimingThread.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/core/backups/BackupTimingThread.java
@@ -1,5 +1,6 @@
 package co.uk.mommyheather.advancedbackups.core.backups;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import co.uk.mommyheather.advancedbackups.core.config.AVConfig;
@@ -7,10 +8,15 @@ import co.uk.mommyheather.advancedbackups.core.config.AVConfig;
 public class BackupTimingThread extends Thread {
     private int loops = 0;
     private int index = 0;
-    private long startTime = 0;
+    private long prev = 0;
+
+    public BackupTimingThread() {
+        setName("AB Backup Timing Daemon");
+        setDaemon(true);
+    }
+
     @Override
     public void run() {
-        startTime = System.currentTimeMillis();
         while (true) {
             try {
                 long time = calculateNextBackupTime();
@@ -27,17 +33,39 @@ public class BackupTimingThread extends Thread {
 
     private long calculateNextBackupTime() {
         long forcedMillis = BackupWrapper.mostRecentBackupTime() + (long) (AVConfig.config.getMaxTimer() * 3600000L);
-
-        ArrayList<Long> timings = new ArrayList<Long>(BackupWrapper.configuredPlaytime);
-        if (index >= timings.size()) {
-            index = 0;
-            loops++;
+        long ret = Long.MAX_VALUE;
+        if (AVConfig.config.getUptimeSchedule() && !BackupWrapper.configuredPlaytime.isEmpty()) {
+            ArrayList<Long> timings = new ArrayList<Long>(BackupWrapper.configuredPlaytime);
+            if (index >= timings.size()) {
+                index = 0;
+                loops++;
+            }
+            ret = (timings.get(index) + (timings.get(timings.size() - 1) * loops));
+            ret -= prev; 
+            prev += ret;
+            index++;
         }
+
+        else if (!BackupWrapper.configuredPlaytime.isEmpty()) {
+            long nextTime = 0;
+            long currentTime = (LocalDateTime.now().getHour() * 3600000) + (LocalDateTime.now().getMinute() * 60000);
+            ArrayList<Long> timings = new ArrayList<Long>(BackupWrapper.configuredPlaytime);
+            for (long time : timings) {
+                if (time >= currentTime) {
+                    nextTime = time;
+                    break;
+                }
+            }
+            ret = nextTime >= currentTime ? nextTime - currentTime : 86640000 - currentTime;
+        }
+
+        return Math.min(forcedMillis, ret);
+
+
+
         
 
-        long nextScheduled = timings.size() >= 1 ? (timings.get(index) + (timings.get(timings.size()-1) * loops)) : Long.MAX_VALUE;
-        index++;
-        return Math.min(forcedMillis, nextScheduled);
+
     }
 
     
