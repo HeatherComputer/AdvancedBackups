b5f7a72da3b0cfc37b88fb8d573a2cc1355a1a24
Heather White <heather@heather.computer>
2023-05-18 19:33:13 +0100

    Initial commit to match v0.1

---
+++ src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackups.java
@@ -0,0 +1,55 @@
+package co.uk.mommyheather.advancedbackups;
+
+
+import co.uk.mommyheather.advancedbackups.core.backups.BackupWrapper;
+import co.uk.mommyheather.advancedbackups.core.config.AVConfig;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
+
+import java.util.function.Consumer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("advancedbackups")
+public class AdvancedBackups
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final Consumer<String> infoLogger = LOGGER::info;
+    public static final Consumer<String> warningLogger = LOGGER::warn;
+    public static final Consumer<String> errorLogger = LOGGER::error;
+
+
+    public AdvancedBackups()
+    {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event)
+    {
+        // Do something when the server starts
+        AVConfig.loadOrCreateConfig();
+        LOGGER.info("Config loaded!!");
+        PlatformMethodWrapper.worldName = event.getServer().getWorldData().getLevelName();
+        PlatformMethodWrapper.worldDir = event.getServer().storageSource.getWorldDir();
+        
+    }
+
+    @SubscribeEvent
+    public void onServerStarted(FMLServerStartedEvent event) {
+        BackupWrapper.checkStartupBackups();
+    }
+
+    @SubscribeEvent
+    public void onServerStopping(FMLServerStoppingEvent event) {
+        BackupWrapper.checkShutdownBackups();
+    }
+
+}
