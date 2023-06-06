642f7ae5263c7a79e11029cab381ede1cab1bf74
Heather White <heather@heather.computer>
2023-06-06 17:12:43 +0100

    Support player activity requirements.

--- src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackups.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackups.java
@@ -3,6 +3,7 @@ package co.uk.mommyheather.advancedbackups;
 import net.minecraft.init.Blocks;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -10,6 +11,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 import org.apache.logging.log4j.Logger;
@@ -23,11 +25,12 @@ import java.util.function.Consumer;
 
 
 @Mod(modid = AdvancedBackups.MODID, name = AdvancedBackups.NAME, version = AdvancedBackups.VERSION, acceptableRemoteVersions = "*")
+@EventBusSubscriber
 public class AdvancedBackups
 {
     public static final String MODID = "advancedbackups";
     public static final String NAME = "Advanced Backups";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.3";
 
     private static Logger LOGGER;
     public static Consumer<String> infoLogger;
@@ -79,6 +82,11 @@ public class AdvancedBackups
     @EventHandler
     public void onServerStopping(FMLServerStoppingEvent event) {
         BackupWrapper.checkShutdownBackups();
+    }
+
+    @SubscribeEvent
+    public void onPlayerConnect(PlayerEvent.PlayerLoggedInEvent event) {
+        PlatformMethodWrapper.activity = true;
     }
 
 }
