361cb4ba6aa53605ac36a1c292a594085524bcbd
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-06-07 15:32:17 +0100

    Small refactors and support for a command system.

---
+++ src/main/java/co/uk/mommyheather/advancedbackups/core/backups/BackupCheckEnum.java
@@ -0,0 +1,38 @@
+package co.uk.mommyheather.advancedbackups.core.backups;
+
+public enum BackupCheckEnum {
+    SUCCESS,
+    DISABLED,
+    NOACTIVITY,
+    TOORECENT;
+
+    public boolean success() {
+        switch (this) {
+            case SUCCESS: {
+                return true;
+            }
+            default : return false;
+        }
+    }
+
+
+    public String getCheckMessage() {
+        switch(this) {
+            case SUCCESS : {
+                return "Checks successful!";
+            }
+            case DISABLED : {
+                return "Backups are disabled!";
+            }
+            case NOACTIVITY : {
+                return "Player activity is required, but none have been active!";
+            }
+            case TOORECENT : {
+                return "The time since the last backup is less than the minimum time specified in config!";
+            }
+            default : {
+                return "You should not see this message, report a bug!";
+            }
+        }
+    }
+}
