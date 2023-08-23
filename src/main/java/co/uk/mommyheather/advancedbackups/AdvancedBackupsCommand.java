ba278176a1ee951d22138eba5821d403f2d60df7
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-08-23 19:03:22 +0100

    1.19 forge

--- src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackupsCommand.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackupsCommand.java
@@ -5,7 +5,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import co.uk.mommyheather.advancedbackups.core.CoreCommandSystem;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 
 public class AdvancedBackupsCommand {
     public static void register(CommandDispatcher<CommandSourceStack> stack) {
@@ -13,35 +13,35 @@ public class AdvancedBackupsCommand {
             return runner.hasPermission(3);
         }).then(Commands.literal("check").executes((runner) -> {
             CoreCommandSystem.checkBackups((response) -> {
-                runner.getSource().sendSuccess(new TextComponent(response), true);
+                runner.getSource().sendSuccess(Component.literal(response), true);
             });
             return 1;
          }))
          
          .then(Commands.literal("start").executes((runner) -> {
             CoreCommandSystem.startBackup((response) -> {
-                runner.getSource().sendSuccess(new TextComponent(response), true);
+                runner.getSource().sendSuccess(Component.literal(response), true);
             });
             return 1;
          }))
 
          .then(Commands.literal("force-backup").executes((runner) -> {
             CoreCommandSystem.forceBackup((response) -> {
-                runner.getSource().sendSuccess(new TextComponent(response), true);
+                runner.getSource().sendSuccess(Component.literal(response), true);
             });
             return 1;
          }))
 
          .then(Commands.literal("reload").executes((runner) -> {
             CoreCommandSystem.reloadConfig((response) -> {
-                runner.getSource().sendSuccess(new TextComponent(response), true);
+                runner.getSource().sendSuccess(Component.literal(response), true);
             });
             return 1;
          }))
 
          .then(Commands.literal("reset-chain").executes((runner) -> {
             CoreCommandSystem.resetChainLength((response) -> {
-                runner.getSource().sendSuccess(new TextComponent(response), true);
+                runner.getSource().sendSuccess(Component.literal(response), true);
             });
             return 1;
          }))
