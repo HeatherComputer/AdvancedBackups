5b6fb56dcb98d2487bc3abc6ac96cac2cd46aaa6
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-09-20 18:55:44 +0100

    Cleanup BackupWrapper

--- src/main/java/co/uk/mommyheather/advancedbackups/core/backups/BackupWrapper.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/core/backups/BackupWrapper.java
@@ -5,8 +5,6 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Date;
