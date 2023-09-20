9b680c36d1822cf1dc60d791d37132edd71aca20
Heather White <heather@heather.computer>
2023-09-20 14:48:56 +0100

    adjust corelib dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,8 +106,8 @@ dependencies {
   // Adds NotEnoughItems and its dependencies (CCL&CCC) to runClient/runServer
   runtimeOnlyNonPublishable("com.github.GTNewHorizons:NotEnoughItems:2.3.39-GTNH:dev")
   
-  implementation  (files("../advancedbackups-corelib.jar"))
-  extraLibs (files("../advancedbackups-corelib.jar"))
+  implementation  (files("advancedbackups-corelib.jar"))
+  extraLibs (files("advancedbackups-corelib.jar"))
   // Example: grab the ic2 jar from curse maven and deobfuscate
   // api(rfg.deobf("curse.maven:ic2-242638:2353971"))
   // Example: grab the ic2 jar from libs/ in the workspace and deobfuscate
