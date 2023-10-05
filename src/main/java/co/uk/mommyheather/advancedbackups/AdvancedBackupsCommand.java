5c3972c0a66df566fd3b5ee4a00a47bc62e8fe56
Heather White <heather@heather.computer>
2023-10-05 17:30:48 +0100

    support 2.2 command changes

--- src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackupsCommand.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackupsCommand.java
@@ -9,28 +9,10 @@ import net.minecraft.network.chat.Component;
 
 public class AdvancedBackupsCommand {
     public static void register(CommandDispatcher<CommandSourceStack> stack) {
-        stack.register(Commands.literal("advancedbackups").requires((runner) -> {
+        stack.register(Commands.literal("backup").requires((runner) -> {
             return runner.hasPermission(3);
-        }).then(Commands.literal("check").executes((runner) -> {
-            CoreCommandSystem.checkBackups((response) -> {
-                runner.getSource().sendSuccess(() -> { 
-                    return Component.literal(response) ;
-                }, true);
-            });
-            return 1;
-         }))
-         
-         .then(Commands.literal("start").executes((runner) -> {
+        }).then(Commands.literal("start").executes((runner) -> {
             CoreCommandSystem.startBackup((response) -> {
-                runner.getSource().sendSuccess(() -> { 
-                    return Component.literal(response) ;
-                }, true);
-            });
-            return 1;
-         }))
-
-         .then(Commands.literal("force-backup").executes((runner) -> {
-            CoreCommandSystem.forceBackup((response) -> {
                 runner.getSource().sendSuccess(() -> { 
                     return Component.literal(response) ;
                 }, true);
@@ -49,6 +31,15 @@ public class AdvancedBackupsCommand {
 
          .then(Commands.literal("reset-chain").executes((runner) -> {
             CoreCommandSystem.resetChainLength((response) -> {
+                runner.getSource().sendSuccess(() -> { 
+                    return Component.literal(response) ;
+                }, true);
+            });
+            return 1;
+         }))
+
+         .then(Commands.literal("snapshot").executes((runner) -> {
+            CoreCommandSystem.snapshot((response) -> {
                 runner.getSource().sendSuccess(() -> { 
                     return Component.literal(response) ;
                 }, true);
