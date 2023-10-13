96b874cd61d3e78d1c7b28f86291c0dceacf8043
Heather White <66441550+MommyHeather@users.noreply.github.com>
2023-10-13 18:10:57 +0100

    fix a bug which serialised to 12 hr, causing some older backups to be overwritten

--- src/main/java/co/uk/mommyheather/advancedbackups/core/ABCore.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/core/ABCore.java
@@ -76,7 +76,7 @@ public class ABCore {
 
     public static String serialiseBackupName(String in) {
         Date date = new Date();
-        String pattern = "yyyy-MM-dd_hh-mm-ss";
+        String pattern = "yyyy-MM-dd_HH-mm-ss";
         
         return in + "_" + new SimpleDateFormat(pattern).format(date);
     }
