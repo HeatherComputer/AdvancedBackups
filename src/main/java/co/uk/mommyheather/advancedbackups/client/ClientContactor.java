156b8cc4098fe8cd3c09faf9dceaaa5dd2d5af8c
Heather White <66441550+MommyHeather@users.noreply.github.com>
2023-10-23 00:53:06 +0100

    and this is forge 1.20!

---
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ClientContactor.java
@@ -0,0 +1,61 @@
+package co.uk.mommyheather.advancedbackups.client;
+
+import java.util.List;
+
+import co.uk.mommyheather.advancedbackups.interfaces.IClientContactor;
+import co.uk.mommyheather.advancedbackups.network.NetworkHandler;
+import co.uk.mommyheather.advancedbackups.network.PacketBackupStatus;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.server.ServerLifecycleHooks;
+
+public class ClientContactor implements IClientContactor {
+    
+    @Override
+    public void backupComplete() {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        List<ServerPlayer> players = server.getPlayerList().getPlayers();
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, false, true, 0, 0);
+        for (ServerPlayer player : players) {
+            if (player.hasPermissions(3)) {
+                NetworkHandler.sendToClient(player, packet);
+            }
+        }
+    }
+
+    @Override
+    public void backupFailed() {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        List<ServerPlayer> players = server.getPlayerList().getPlayers();
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, true, false, 0, 0);
+        for (ServerPlayer player : players) {
+            if (player.hasPermissions(3)) {
+                NetworkHandler.sendToClient(player, packet);
+            }
+        }
+    }
+
+    @Override
+    public void backupProgress(int progress, int max) {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        List<ServerPlayer> players = server.getPlayerList().getPlayers();
+        PacketBackupStatus packet = new PacketBackupStatus(false, true, false, false, progress, max);
+        for (ServerPlayer player : players) {
+            if (player.hasPermissions(3)) {
+                NetworkHandler.sendToClient(player, packet);
+            }
+        }
+    }
+
+    @Override
+    public void backupStarting() {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        List<ServerPlayer> players = server.getPlayerList().getPlayers();
+        PacketBackupStatus packet = new PacketBackupStatus(true, false, false, false, 0, 0);
+        for (ServerPlayer player : players) {
+            if (player.hasPermissions(3)) {
+                NetworkHandler.sendToClient(player, packet);
+            }
+        }
+    }
+}
