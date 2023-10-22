f7f41b168f60ddcf7012f1e7ecb27aefb0470716
Heather White <66441550+MommyHeather@users.noreply.github.com>
2023-10-22 21:43:26 +0100

    Client contact, with progress bar, 1.19 fabric!

---
+++ src/main/java/co/uk/mommyheather/advancedbackups/network/PacketBackupStatus.java
@@ -0,0 +1,83 @@
+package co.uk.mommyheather.advancedbackups.network;
+
+import co.uk.mommyheather.advancedbackups.client.BackupToast;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.PacketByteBuf;
+
+public class PacketBackupStatus {
+    
+    public boolean starting;
+    public boolean started;
+    public boolean failed;
+    public boolean finished;
+
+    public int progress;
+    public int max;
+
+
+    public PacketBackupStatus(boolean starting, boolean started, boolean failed, boolean finished, int progress,
+            int max) {
+        this.starting = starting;
+        this.started = started;
+        this.failed = failed;
+        this.finished = finished;
+        this.progress = progress;
+        this.max = max;
+    }
+
+    public PacketBackupStatus() {
+
+    }
+
+    public void read(PacketByteBuf buf) {
+        starting = buf.readBoolean();
+        started = buf.readBoolean();
+        failed = buf.readBoolean();
+        finished = buf.readBoolean();
+
+        progress = buf.readInt();
+        max = buf.readInt();
+    }
+
+    public PacketByteBuf write(PacketByteBuf buf) {
+        buf.writeBoolean(starting);
+        buf.writeBoolean(started);
+        buf.writeBoolean(failed);
+        buf.writeBoolean(finished);
+
+        buf.writeInt(progress);
+        buf.writeInt(max);
+
+        return buf;
+
+    }
+
+
+
+    public static void handle(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
+
+        PacketBackupStatus message = new PacketBackupStatus();
+        message.read(buf);
+
+        client.execute(() -> {
+            BackupToast.starting = message.starting;
+            BackupToast.started = message.started;
+            BackupToast.failed = message.failed;
+            BackupToast.finished = message.finished;
+    
+            BackupToast.progress = message.progress;
+            BackupToast.max = message.max;
+    
+            if (!BackupToast.exists) {
+                BackupToast.exists = true;
+                client.getToastManager().add(new BackupToast());
+            }
+
+        });
+
+
+    }
+    
+    }
