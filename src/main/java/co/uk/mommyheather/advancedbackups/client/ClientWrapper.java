cfaf01aef894319247b7d4c26ec95867d6c967ab
Heather White <heather@heather.computer>
2023-10-24 21:57:11 +0100

    support 1.20.2 forge

--- src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
@@ -5,7 +5,16 @@ import net.minecraft.client.Minecraft;
 
 public class ClientWrapper {
 
-    public static void handle(PacketBackupStatus packetBackupStatus) {
+    public static void handle(PacketBackupStatus message) {
+        
+        BackupToast.starting = message.starting;
+        BackupToast.started = message.started;
+        BackupToast.failed = message.failed;
+        BackupToast.finished = message.finished;
+
+        BackupToast.progress = message.progress;
+        BackupToast.max = message.max;
+
         if (!BackupToast.exists) {
             BackupToast.exists = true;
             Minecraft.getInstance().getToasts().addToast(new BackupToast());
