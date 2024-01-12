a9d052c2bf93b5dabf1ee011adbe02f2c633f7cc
Heather White <66441550+MommyHeather@users.noreply.github.com>
2024-01-12 12:44:54 +0000

    Add extra method to `IClientContactor`. This needs version-specific implementation.

--- src/main/java/co/uk/mommyheather/advancedbackups/interfaces/IClientContactor.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/interfaces/IClientContactor.java
@@ -5,4 +5,5 @@ public interface IClientContactor {
     public void backupProgress(int current, int max);
     public void backupComplete();
     public void backupFailed();
+    public void backupCancelled();
 }
