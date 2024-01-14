e2f707c3221470248443e661f7b05f4b88539bf6
Heather White <66441550+MommyHeather@users.noreply.github.com>
2024-01-14 23:55:16 +0000

    more clientside config + toast subscription stuff

--- src/main/java/co/uk/mommyheather/advancedbackups/client/ABClientContactor.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ABClientContactor.java
@@ -19,10 +19,11 @@ public class ABClientContactor implements IClientContactor {
     @Override
     public void backupComplete() {
         ServerConfigurationManager configurationManager = AdvancedBackups.server.getConfigurationManager();
-        PacketBackupStatus packet = new PacketBackupStatus(false, false, false, true, 0, 0);
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, false, true, false, 0, 0);
         configurationManager.playerEntityList.forEach((player) -> {
             if (player instanceof EntityPlayerMP) {
                 EntityPlayerMP playerMP = (EntityPlayerMP) player;
+                if (!AdvancedBackups.players.contains(playerMP.getGameProfile().getId().toString())) return;
                 //if they can run the command, or are in singleplayer, they should receive info on active backups.
                 if (playerMP.canCommandSenderUseCommand(3, "advancedbackups") || !AdvancedBackups.server.isDedicatedServer()) {
                     NetworkHandler.HANDLER.sendTo(packet, playerMP);
@@ -34,10 +35,11 @@ public class ABClientContactor implements IClientContactor {
     @Override
     public void backupFailed() {
         ServerConfigurationManager configurationManager = AdvancedBackups.server.getConfigurationManager();
-        PacketBackupStatus packet = new PacketBackupStatus(false, false, true, false, 0, 0);
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, true, false, false, 0, 0);
         configurationManager.playerEntityList.forEach((player) -> {
             if (player instanceof EntityPlayerMP) {
                 EntityPlayerMP playerMP = (EntityPlayerMP) player;
+                if (!AdvancedBackups.players.contains(playerMP.getGameProfile().getId().toString())) return;
                 if (playerMP.canCommandSenderUseCommand(3, "advancedbackups")) { //if they can run the command, they should receive info on active backups
                     NetworkHandler.HANDLER.sendTo(packet, playerMP);
                 }
@@ -48,10 +50,11 @@ public class ABClientContactor implements IClientContactor {
     @Override
     public void backupProgress(int progress, int max) {
         ServerConfigurationManager configurationManager = AdvancedBackups.server.getConfigurationManager();
-        PacketBackupStatus packet = new PacketBackupStatus(false, true, false, false, progress, max);
+        PacketBackupStatus packet = new PacketBackupStatus(false, true, false, false, false, progress, max);
         configurationManager.playerEntityList.forEach((player) -> {
             if (player instanceof EntityPlayerMP) {
                 EntityPlayerMP playerMP = (EntityPlayerMP) player;
+                if (!AdvancedBackups.players.contains(playerMP.getGameProfile().getId().toString())) return;
                 if (playerMP.canCommandSenderUseCommand(3, "advancedbackups")) { //if they can run the command, they should receive info on active backups
                     NetworkHandler.HANDLER.sendTo(packet, playerMP);
                 }
@@ -62,10 +65,26 @@ public class ABClientContactor implements IClientContactor {
     @Override
     public void backupStarting() {
         ServerConfigurationManager configurationManager = AdvancedBackups.server.getConfigurationManager();
-        PacketBackupStatus packet = new PacketBackupStatus(true, false, false, false, 0, 0);
+        PacketBackupStatus packet = new PacketBackupStatus(true, false, false, false, false, 0, 0);
         configurationManager.playerEntityList.forEach((player) -> {
             if (player instanceof EntityPlayerMP) {
                 EntityPlayerMP playerMP = (EntityPlayerMP) player;
+                if (!AdvancedBackups.players.contains(playerMP.getGameProfile().getId().toString())) return;
+                if (playerMP.canCommandSenderUseCommand(3, "advancedbackups")) { //if they can run the command, they should receive info on active backups
+                    NetworkHandler.HANDLER.sendTo(packet, playerMP);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void backupCancelled() {
+        ServerConfigurationManager configurationManager = AdvancedBackups.server.getConfigurationManager();
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, false, false, true, 0, 0);
+        configurationManager.playerEntityList.forEach((player) -> {
+            if (player instanceof EntityPlayerMP) {
+                EntityPlayerMP playerMP = (EntityPlayerMP) player;
+                if (!AdvancedBackups.players.contains(playerMP.getGameProfile().getId().toString())) return;
                 if (playerMP.canCommandSenderUseCommand(3, "advancedbackups")) { //if they can run the command, they should receive info on active backups
                     NetworkHandler.HANDLER.sendTo(packet, playerMP);
                 }
--- src/main/java/co/uk/mommyheather/advancedbackups/network/PacketBackupStatus.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/network/PacketBackupStatus.java
@@ -11,17 +11,19 @@ public class PacketBackupStatus implements IMessage{
     public boolean started;
     public boolean failed;
     public boolean finished;
+    public boolean cancelled;
 
     public int progress;
     public int max;
 
 
-    public PacketBackupStatus(boolean starting, boolean started, boolean failed, boolean finished, int progress,
+    public PacketBackupStatus(boolean starting, boolean started, boolean failed, boolean finished, boolean cancelled, int progress,
             int max) {
         this.starting = starting;
         this.started = started;
         this.failed = failed;
         this.finished = finished;
+        this.cancelled = cancelled;
         this.progress = progress;
         this.max = max;
     }
@@ -36,6 +38,7 @@ public class PacketBackupStatus implements IMessage{
         started = buf.readBoolean();
         failed = buf.readBoolean();
         finished = buf.readBoolean();
+        cancelled = buf.readBoolean();
 
         progress = buf.readInt();
         max = buf.readInt();
@@ -47,6 +50,7 @@ public class PacketBackupStatus implements IMessage{
         buf.writeBoolean(started);
         buf.writeBoolean(failed);
         buf.writeBoolean(finished);
+        buf.writeBoolean(cancelled);
 
         buf.writeInt(progress);
         buf.writeInt(max);
@@ -63,6 +67,7 @@ public class PacketBackupStatus implements IMessage{
             ABClientRenderer.started = message.started;
             ABClientRenderer.failed = message.failed;
             ABClientRenderer.finished = message.finished;
+            ABClientRenderer.cancelled = message.cancelled;
 
             ABClientRenderer.progress = message.progress;
             ABClientRenderer.max = message.max;
