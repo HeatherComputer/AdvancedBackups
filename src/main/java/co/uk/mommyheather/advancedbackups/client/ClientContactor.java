b3eb8b568da2917dd4b14a418c7aa3252c09ac30
Heather White <66441550+MommyHeather@users.noreply.github.com>
2024-02-05 21:31:25 +0000

    3.5, for fabric 1.19.3/4!

--- src/main/java/co/uk/mommyheather/advancedbackups/client/ClientContactor.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ClientContactor.java
@@ -15,9 +15,10 @@ public class ClientContactor implements IClientContactor {
     public void backupComplete() {
         MinecraftServer server = AdvancedBackups.server;
         List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
-        PacketBackupStatus packet = new PacketBackupStatus(false, false, false, true, 0, 0);
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, false, true, false, 0, 0);
         for (ServerPlayerEntity player : players) {
-            if (!AdvancedBackups.server.isDedicated() || player.hasPermissionLevel(3)) {
+            if (!AdvancedBackups.players.contains(player.getUuidAsString())) continue;
+            if (!server.isDedicated() || player.hasPermissionLevel(3)) {
                 NetworkHandler.sendToClient(player, packet);
             }
         }
@@ -27,9 +28,10 @@ public class ClientContactor implements IClientContactor {
     public void backupFailed() {
         MinecraftServer server = AdvancedBackups.server;
         List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
-        PacketBackupStatus packet = new PacketBackupStatus(false, false, true, false, 0, 0);
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, true, false, false, 0, 0);
         for (ServerPlayerEntity player : players) {
-            if (!AdvancedBackups.server.isDedicated() || player.hasPermissionLevel(3)) {
+            if (!AdvancedBackups.players.contains(player.getUuidAsString())) continue;
+            if (!server.isDedicated() || player.hasPermissionLevel(3)) {
                 NetworkHandler.sendToClient(player, packet);
             }
         }
@@ -39,9 +41,10 @@ public class ClientContactor implements IClientContactor {
     public void backupProgress(int progress, int max) {
         MinecraftServer server = AdvancedBackups.server;
         List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
-        PacketBackupStatus packet = new PacketBackupStatus(false, true, false, false, progress, max);
+        PacketBackupStatus packet = new PacketBackupStatus(false, true, false, false, false, progress, max);
         for (ServerPlayerEntity player : players) {
-            if (!AdvancedBackups.server.isDedicated() || player.hasPermissionLevel(3)) {
+            if (!AdvancedBackups.players.contains(player.getUuidAsString())) continue;
+            if (!server.isDedicated() || player.hasPermissionLevel(3)) {
                 NetworkHandler.sendToClient(player, packet);
             }
         }
@@ -51,9 +54,23 @@ public class ClientContactor implements IClientContactor {
     public void backupStarting() {
         MinecraftServer server = AdvancedBackups.server;
         List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
-        PacketBackupStatus packet = new PacketBackupStatus(true, false, false, false, 0, 0);
+        PacketBackupStatus packet = new PacketBackupStatus(true, false, false, false, false, 0, 0);
         for (ServerPlayerEntity player : players) {
-            if (!AdvancedBackups.server.isDedicated() || player.hasPermissionLevel(3)) {
+            if (!AdvancedBackups.players.contains(player.getUuidAsString())) continue;
+            if (!server.isDedicated() || player.hasPermissionLevel(3)) {
+                NetworkHandler.sendToClient(player, packet);
+            }
+        }
+    }
+
+    @Override
+    public void backupCancelled() {        
+        MinecraftServer server = AdvancedBackups.server;
+        List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
+        PacketBackupStatus packet = new PacketBackupStatus(false, false, false, false, true, 0, 0);
+        for (ServerPlayerEntity player : players) {
+            if (!AdvancedBackups.players.contains(player.getUuidAsString())) continue;
+            if (!server.isDedicated() || player.hasPermissionLevel(3)) {
                 NetworkHandler.sendToClient(player, packet);
             }
         }
--- src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
@@ -1,19 +1,47 @@
 package co.uk.mommyheather.advancedbackups.client;
 
+import co.uk.mommyheather.advancedbackups.AdvancedBackups;
+import co.uk.mommyheather.advancedbackups.core.ABCore;
+import co.uk.mommyheather.advancedbackups.core.config.ClientConfigManager;
 import co.uk.mommyheather.advancedbackups.network.NetworkHandler;
 import co.uk.mommyheather.advancedbackups.network.PacketBackupStatus;
+import co.uk.mommyheather.advancedbackups.network.PacketToastSubscribe;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.PacketByteBuf;
 
 public class ClientWrapper implements ClientModInitializer {
-
+    
     @Override
     public void onInitializeClient() {
+
+        ABCore.infoLogger = AdvancedBackups.infoLogger;
+        ABCore.warningLogger = AdvancedBackups.warningLogger;
+        ABCore.errorLogger = AdvancedBackups.errorLogger;
+
         ClientPlayNetworking.registerGlobalReceiver(NetworkHandler.STATUS_PACKET_ID, ClientWrapper::handle);
+        ClientLifecycleEvents.CLIENT_STARTED.register((client) -> {
+            ClientConfigManager.loadOrCreateConfig();
+        });
+
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            PacketToastSubscribe packet = new PacketToastSubscribe(ClientConfigManager.showProgress.get());
+            sender.sendPacket(NetworkHandler.TOAST_SUBSCRIBE_ID, packet.write(PacketByteBufs.create()));
+        });
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            AdvancedBackupsClientCommand.register(dispatcher);
+
+        });
+
+        
     }
 
     
@@ -28,6 +56,7 @@ public class ClientWrapper implements ClientModInitializer {
             BackupToast.started = message.started;
             BackupToast.failed = message.failed;
             BackupToast.finished = message.finished;
+            BackupToast.cancelled = message.cancelled;
     
             BackupToast.progress = message.progress;
             BackupToast.max = message.max;
