24870c3f6b359c489e71bac0afa9b48d83515609
Heather White <heather@heather.computer>
2024-02-22 21:58:17 +0000

    new client events and storage of players to send toasts to

--- src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
@@ -1,7 +1,14 @@
 package co.uk.mommyheather.advancedbackups.client;
 
+import co.uk.mommyheather.advancedbackups.core.config.ClientConfigManager;
+import co.uk.mommyheather.advancedbackups.network.NetworkHandler;
 import co.uk.mommyheather.advancedbackups.network.PacketBackupStatus;
+import co.uk.mommyheather.advancedbackups.network.PacketToastSubscribe;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.client.event.RegisterClientCommandsEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ClientWrapper {
 
@@ -10,6 +17,7 @@ public class ClientWrapper {
         BackupToast.started = packet.started;
         BackupToast.failed = packet.failed;
         BackupToast.finished = packet.finished;
+        BackupToast.cancelled = packet.cancelled;
 
         BackupToast.progress = packet.progress;
         BackupToast.max = packet.max;
@@ -18,6 +26,20 @@ public class ClientWrapper {
             BackupToast.exists = true;
             Minecraft.getInstance().getToasts().addToast(new BackupToast());
         }
+    }
+
+    public static void init(FMLClientSetupEvent e) {
+        MinecraftForge.EVENT_BUS.addListener(ClientWrapper::registerClientCommands);
+        MinecraftForge.EVENT_BUS.addListener(ClientWrapper::onServerConnected);
+        ClientConfigManager.loadOrCreateConfig();
+    }
+
+    public static void registerClientCommands(RegisterClientCommandsEvent event) {
+        AdvancedBackupsClientCommand.register(event.getDispatcher());
+    }
+
+    public static void onServerConnected(ClientPlayerNetworkEvent.LoggingIn event) {
+        NetworkHandler.sendToServer(new PacketToastSubscribe(ClientConfigManager.showProgress.get()));
     }
     
 }
