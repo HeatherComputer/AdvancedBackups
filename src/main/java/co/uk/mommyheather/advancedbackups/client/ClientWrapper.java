d158c8036e27ae1579744cc914c4e4f7616cef23
Heather White <66441550+MommyHeather@users.noreply.github.com>
2024-03-13 18:57:03 +0000

    Support client config reloading

--- src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ClientWrapper.java
@@ -1,7 +1,13 @@
 package co.uk.mommyheather.advancedbackups.client;
 
+import co.uk.mommyheather.advancedbackups.core.config.ClientConfigManager;
+import co.uk.mommyheather.advancedbackups.network.NetworkHandler;
 import co.uk.mommyheather.advancedbackups.network.PacketBackupStatus;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.client.event.RegisterClientCommandsEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ClientWrapper {
 
@@ -21,6 +27,20 @@ public class ClientWrapper {
             BackupToast.exists = true;
             Minecraft.getInstance().getToasts().addToast(new BackupToast());
         }
+    }
+
+    public static void init(FMLClientSetupEvent e) {
+        MinecraftForge.EVENT_BUS.addListener(ClientWrapper::registerClientCommands);
+        MinecraftForge.EVENT_BUS.addListener(ClientWrapper::onServerConnected);
+        ClientConfigManager.loadOrCreateConfig();
+    }
+
+    public static void registerClientCommands(RegisterClientCommandsEvent event) {
+        AdvancedBackupsClientCommand.register(event.getDispatcher());
+    }
+
+    public static void onServerConnected(ClientPlayerNetworkEvent.LoggingIn event) {
+        NetworkHandler.sendToServer(new PacketToastSubscribe(ClientConfigManager.showProgress.get()));
     }
     
 }
