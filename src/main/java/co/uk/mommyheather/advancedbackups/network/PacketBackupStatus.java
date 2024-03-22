ed1ad58d4b14f2d534a5d6a2b15011257e0f018b
Heather White <heather@heather.computer>
2024-03-22 18:37:24 +0000

    support v3.5 changes on 1.20.2

--- src/main/java/co/uk/mommyheather/advancedbackups/network/PacketBackupStatus.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/network/PacketBackupStatus.java
@@ -1,9 +1,5 @@
 package co.uk.mommyheather.advancedbackups.network;
 
-import co.uk.mommyheather.advancedbackups.client.BackupToast;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.PacketByteBuf;
 
 public class PacketBackupStatus {
@@ -12,17 +8,19 @@ public class PacketBackupStatus {
     public boolean started;
     public boolean failed;
     public boolean finished;
+    public boolean cancelled;
 
     public int progress;
     public int max;
 
 
-    public PacketBackupStatus(boolean starting, boolean started, boolean failed, boolean finished, int progress,
+    public PacketBackupStatus(boolean starting, boolean started, boolean failed, boolean finished, boolean cancelled, int progress,
             int max) {
         this.starting = starting;
         this.started = started;
         this.failed = failed;
         this.finished = finished;
+        this.cancelled = cancelled;
         this.progress = progress;
         this.max = max;
     }
@@ -36,6 +34,7 @@ public class PacketBackupStatus {
         started = buf.readBoolean();
         failed = buf.readBoolean();
         finished = buf.readBoolean();
+        cancelled = buf.readBoolean();
 
         progress = buf.readInt();
         max = buf.readInt();
@@ -46,6 +45,7 @@ public class PacketBackupStatus {
         buf.writeBoolean(started);
         buf.writeBoolean(failed);
         buf.writeBoolean(finished);
+        buf.writeBoolean(cancelled);
 
         buf.writeInt(progress);
         buf.writeInt(max);
@@ -54,4 +54,5 @@ public class PacketBackupStatus {
 
     }
 
+    
 }
