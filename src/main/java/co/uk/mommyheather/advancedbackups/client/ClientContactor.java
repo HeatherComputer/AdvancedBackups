51c5b54861e101e839114b53b46a4812aa2dcfc7
Heather White <heather@heather.computer>
2024-06-17 00:02:17 +0100

    support v3.6 changes

--- src/main/java/co/uk/mommyheather/advancedbackups/client/ClientContactor.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/client/ClientContactor.java
@@ -11,60 +11,60 @@ import net.minecraft.entity.player.EntityPlayerMP;
 public class ClientContactor implements IClientContactor {
 
     @Override
-    public void backupComplete() {
+    public void backupComplete(boolean all) {
         List<EntityPlayerMP> players = AdvancedBackups.server.getPlayerList().getPlayers();
         PacketBackupStatus packet = new PacketBackupStatus(false, false, false, true, false, 0, 0);
         for (EntityPlayerMP player : players) {
             if (!AdvancedBackups.players.contains(player.getGameProfile().getId().toString())) continue;
-            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup")) {
+            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup") || all) {
                 NetworkHandler.HANDLER.sendTo(packet, player);
             }
         }
     }
 
     @Override
-    public void backupFailed() {
+    public void backupFailed(boolean all) {
         List<EntityPlayerMP> players = AdvancedBackups.server.getPlayerList().getPlayers();
         PacketBackupStatus packet = new PacketBackupStatus(false, false, true, false, false, 0, 0);
         for (EntityPlayerMP player : players) {
             if (!AdvancedBackups.players.contains(player.getGameProfile().getId().toString())) continue;
-            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup")) {
+            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup") || all) {
                 NetworkHandler.HANDLER.sendTo(packet, player);
             }
         }
     }
 
     @Override
-    public void backupProgress(int progress, int max) {
+    public void backupProgress(int progress, int max, boolean all) {
         List<EntityPlayerMP> players = AdvancedBackups.server.getPlayerList().getPlayers();
         PacketBackupStatus packet = new PacketBackupStatus(false, true, false, false, false, progress, max);
         for (EntityPlayerMP player : players) {
             if (!AdvancedBackups.players.contains(player.getGameProfile().getId().toString())) continue;
-            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup")) {
+            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup") || all) {
                 NetworkHandler.HANDLER.sendTo(packet, player);
             }
         }
     }
 
     @Override
-    public void backupStarting() {
+    public void backupStarting(boolean all) {
         List<EntityPlayerMP> players = AdvancedBackups.server.getPlayerList().getPlayers();
         PacketBackupStatus packet = new PacketBackupStatus(true, false, false, false, false, 0, 0);
         for (EntityPlayerMP player : players) {
             if (!AdvancedBackups.players.contains(player.getGameProfile().getId().toString())) continue;
-            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup")) {
+            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup") || all) {
                 NetworkHandler.HANDLER.sendTo(packet, player);
             }
         }
     }
 
     @Override
-    public void backupCancelled() {
+    public void backupCancelled(boolean all) {
         List<EntityPlayerMP> players = AdvancedBackups.server.getPlayerList().getPlayers();
         PacketBackupStatus packet = new PacketBackupStatus(false, false, false, false, true, 0, 0);
         for (EntityPlayerMP player : players) {
             if (!AdvancedBackups.players.contains(player.getGameProfile().getId().toString())) continue;
-            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup")) {
+            if (!AdvancedBackups.server.isDedicatedServer() || player.canUseCommand(3, "backup") || all) {
                 NetworkHandler.HANDLER.sendTo(packet, player);
             }
         }
