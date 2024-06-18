d578a646d7e84ad093c8da6bb1d39bf733033e43
Heather White <66441550+MommyHeather@users.noreply.github.com>
2024-06-18 22:03:04 +0100

    I can't believe I'm copying fucking 1.7.10 code

--- src/main/java/computer/heather/AdvancedBackups/AdvancedBackupsCommand.java
+++ src/main/java/computer/heather/AdvancedBackups/AdvancedBackupsCommand.java
@@ -1,19 +1,96 @@
 package computer.heather.AdvancedBackups;
 
-import java.util.Arrays;
-
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class AdvancedBackupsCommand extends Command {
+import co.uk.mommyheather.advancedbackups.core.CoreCommandSystem;
 
-    public AdvancedBackupsCommand() {
-        super("backup", "/backup start | reload-config | reload-client-config | snapshot | reset-chain", "Commands for the Advanced Backups mod", Arrays.asList(new String[] {"advancedbackups"}));
-    }
+
+// /backup start | reload-config | reload-client-config | snapshot | reset-chain
+public class AdvancedBackupsCommand implements CommandExecutor {
 
     @Override
-    public boolean execute(CommandSender arg0, String arg1, String[] arg2) {
-        return false;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0)
+        {
+            return false;
+        }
+        else if ("start".equals(args[0]))
+        {
+            Start.execute(sender);
+        }
+        else if ("reload-config".equals(args[0]))
+        {
+            Reload.execute(sender);
+        }
+        else if ("reload-client-config".equals(args[0]))
+        {
+            ReloadClient.execute(sender);
+        }
+        else if ("reset-chain".equals(args[0])) {
+            ResetChain.execute(sender);
+        }
+        else if ("snapshot".equals(args[0])) {
+            Snapshot.execute(sender);
+        }
+        else if ("cancel".equals(args[0])) {
+            Cancel.execute(sender);
+        }
+        else
+        {
+            return false;
+        }
+        return true;
     }
     
+    public static class Reload {
+        public static void execute(CommandSender sender) {
+            CoreCommandSystem.reloadConfig((response) -> {
+                sender.sendMessage(response);
+            });
+        }
+    }   
+
+    public static class ReloadClient {
+        public static void execute(CommandSender sender) {
+            sender.sendMessage("This can only be ran on the client!");
+        }
+    }    
+    
+    public static class Start {
+        public static void execute(CommandSender sender) {
+            CoreCommandSystem.startBackup((response) -> {
+                sender.sendMessage(response);
+            });
+        }
+    } 
+    
+    public static class ResetChain {
+        public static void execute(CommandSender sender) {
+            CoreCommandSystem.resetChainLength((response) -> {
+                sender.sendMessage(response);
+            });
+        }
+    }
+
+    public static class Snapshot {
+        public static void execute(CommandSender sender) {
+            CoreCommandSystem.snapshot((response) -> {
+                sender.sendMessage(response);
+            });
+        }
+
+    }
+
+    public static class Cancel {
+        public static void execute(CommandSender sender) {
+            CoreCommandSystem.cancelBackup((response) -> {
+                sender.sendMessage(response);
+            });
+        }
+
+    }
+
+
 }
