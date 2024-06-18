1e57c58e757cdc28665e7aba9b9c864f93645000
Heather White <heather@heather.computer>
2024-06-18 21:26:46 +0100

    Initial commit!

---
+++ src/main/java/computer/heather/AdvancedBackups/AdvancedBackups.java
@@ -0,0 +1,17 @@
+package computer.heather.AdvancedBackups;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AdvancedBackups extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable called! I don't yet know how to handle this...");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable called! I don't yet know how to handle this...");
+    }
+
+
+}
