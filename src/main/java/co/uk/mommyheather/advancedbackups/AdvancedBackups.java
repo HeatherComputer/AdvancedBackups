d57f34a2c8b47e3753880be72cf97108e7d87286
Heather White <66441550+MommyHeather@users.noreply.github.com>
2024-06-18 17:35:16 +0100

    3.6 support

--- src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackups.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/AdvancedBackups.java
@@ -142,7 +142,6 @@ public class AdvancedBackups
                 level.noSave = true;
             }
         }
-        if (ConfigManager.silent.get()) return;
         warningLogger.accept(savesDisabledMessage);
     }
 
@@ -153,14 +152,12 @@ public class AdvancedBackups
                 level.noSave = false;
             }
         }
-        if (ConfigManager.silent.get()) return;
         warningLogger.accept(savesEnabledMessage);
     }
 
     public static void saveOnce(boolean flush) {
         MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
         server.saveEverything(true, flush, true);
-        if (ConfigManager.silent.get()) return;
         warningLogger.accept(saveCompleteMessage);
     }
 
