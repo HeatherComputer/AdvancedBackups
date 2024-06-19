9e973b9adde66195f9aac75194ef81d8bf96ab3f
Heather White <66441550+MommyHeather@users.noreply.github.com>
2024-06-19 08:48:30 +0100

    HAHAHAHAHA I HAVE NETWORKING (and improved debugging)

--- src/main/java/computer/heather/AdvancedBackups/AdvancedBackups.java
+++ src/main/java/computer/heather/AdvancedBackups/AdvancedBackups.java
@@ -1,6 +1,7 @@
 package computer.heather.AdvancedBackups;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.function.Consumer;
 
 import org.bukkit.Server;
@@ -16,15 +17,20 @@ import co.uk.mommyheather.advancedbackups.core.ABCore;
 import co.uk.mommyheather.advancedbackups.core.backups.BackupTimer;
 import co.uk.mommyheather.advancedbackups.core.backups.BackupWrapper;
 import co.uk.mommyheather.advancedbackups.core.config.ConfigManager;
+import computer.heather.AdvancedBackups.network.ClientContactor;
+import computer.heather.AdvancedBackups.network.PacketListener;
 
 public class AdvancedBackups extends JavaPlugin implements Listener {
     
     private static boolean enabled = false;
-    private static Server server = null;
+    public static Server server = null;
     
     public static Consumer<String> infoLogger;
     public static Consumer<String> warningLogger;
     public static Consumer<String> errorLogger;
+
+    //List of players that have asked to see packets
+    public static final ArrayList<String> players = new ArrayList<>();
     
     @Override
     public void onEnable() {
@@ -39,7 +45,6 @@ public class AdvancedBackups extends JavaPlugin implements Listener {
         ABCore.worldName = getServer().getWorlds().get(0).getName();
         ABCore.worldDir = new File(getServer().getWorlds().get(0).getWorldFolder(), "./").toPath(); //need to have this parent because of some forge compatability bullshit.. if i knew how much I'd need to do this I really would've just called getParent on the offending versions
         ABCore.modJar = getFile().getAbsoluteFile();
-        
 
         infoLogger = getLogger()::info;
         warningLogger = getLogger()::warning;
@@ -53,10 +58,13 @@ public class AdvancedBackups extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         ConfigManager.loadOrCreateConfig();
+
+        server.getMessenger().registerIncomingPluginChannel(this, "advancedbackups:toast_subscribe", new PacketListener());
+        server.getMessenger().registerOutgoingPluginChannel(this, "advancedbackups:backup_status");
         
 
-        //NYI
-        //ABCore.clientContactor = new ABClientContactor();
+        ABCore.clientContactor = new ClientContactor();
+
         ABCore.resetActivity = AdvancedBackups::resetActivity;
 
         new BukkitRunnable() {
@@ -91,6 +99,11 @@ public class AdvancedBackups extends JavaPlugin implements Listener {
     
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        /*Might not need to do this
+        //Gets us the actual player class, not bukkit's strange reimpl
+        ServerPlayer player = MinecraftServer.getServer().getPlayerList().getPlayer(event.getPlayer().getUniqueId());
+        player.connection.networkmanager*/
+        
         ABCore.setActivity(true);
     }
 
