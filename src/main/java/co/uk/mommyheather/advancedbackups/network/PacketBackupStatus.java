d1afb9046448585950d703d28ccaf239146098b9
Heather White <heather@heather.computer>
2024-06-21 15:13:40 +0100

    hotfix for a dedicated server incompatability

--- src/main/java/co/uk/mommyheather/advancedbackups/network/PacketBackupStatus.java
+++ src/main/java/co/uk/mommyheather/advancedbackups/network/PacketBackupStatus.java
@@ -1,14 +1,18 @@
 package co.uk.mommyheather.advancedbackups.network;
 
+import co.uk.mommyheather.advancedbackups.client.BackupToast;
+import co.uk.mommyheather.advancedbackups.client.ClientWrapper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 public record PacketBackupStatus(boolean starting, boolean started, boolean failed, boolean finished, boolean cancelled, int progress, int max) implements CustomPacketPayload {
-
+    
     public static final CustomPacketPayload.Type<PacketBackupStatus> ID = new CustomPacketPayload.Type<PacketBackupStatus>(ResourceLocation.parse("advancedbackups:backup_status"));
-
+    
     public static final StreamCodec<FriendlyByteBuf, PacketBackupStatus> CODEC = StreamCodec.of((buf, packet) -> {
         buf.writeBoolean(packet.starting);
         buf.writeBoolean(packet.started);
@@ -17,23 +21,28 @@ public record PacketBackupStatus(boolean starting, boolean started, boolean fail
         buf.writeBoolean(packet.cancelled);
         buf.writeInt(packet.progress);
         buf.writeInt(packet.max);
-        }, 
-        
-        buf -> new PacketBackupStatus(
-            buf.readBoolean(),
-            buf.readBoolean(),
-            buf.readBoolean(),
-            buf.readBoolean(),
-            buf.readBoolean(),
-            buf.readInt(),
-            buf.readInt()
-        ));
-
-
+    }, 
+    
+    buf -> new PacketBackupStatus(
+    buf.readBoolean(),
+    buf.readBoolean(),
+    buf.readBoolean(),
+    buf.readBoolean(),
+    buf.readBoolean(),
+    buf.readInt(),
+    buf.readInt()
+    ));
+    
+    
     @Override
     public CustomPacketPayload.Type<? extends CustomPacketPayload> type() {
         return ID;
     }
-
+    
+    
+    public static void handle(PacketBackupStatus packet, IPayloadContext context) {
+        ClientWrapper.handle(packet, context);
+    }
+    
     
 }
