e51636c0e0a7ad49fcb0090133ac4d7c6775bcbd
Heather White <heather@heather.computer>
2024-11-02 13:34:49 +0000

    add version arg to build-all script (...script isn't even usable though, because of JAVA_HOME issues)

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <version number>"
+    exit 1
+fi
+
 root=$(pwd)
 
 # This will fail silently (but deadly) if there's ever a modloader that's lexicographically before "core"
@@ -9,7 +14,7 @@ find . -maxdepth 3 -type f -name 'build.gradle' | sed -r 's|/[^/]+$||' | sort \
     cd "$folder" || exit 1
     
     # Older Gradle versions are naughty and consume stdin for some reason
-    ./gradlew build < /dev/null
+    ./gradlew -Pversion=$1 build < /dev/null
 
     cd "$root" || exit 1
 done
