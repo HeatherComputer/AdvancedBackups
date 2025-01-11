e08501ceb5fa8b84bb0d13e6aebc68982a5e5767
Heather White <heather@heather.computer>
2025-01-11 16:00:15 +0000

    #101 neo 1.20.6

--- neoforge/1.20.6/src/main/java/computer/heather/advancedbackups/client/AdvancedBackupsClientCommand.java
+++ neoforge/1.20.6/src/main/java/computer/heather/advancedbackups/client/AdvancedBackupsClientCommand.java
@@ -1,11 +1,13 @@
 package computer.heather.advancedbackups.client;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import computer.heather.advancedbackups.core.CoreCommandSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ServerboundChatCommandPacket;
 
@@ -31,7 +33,13 @@ public class AdvancedBackupsClientCommand {
          .then(literal("snapshot").executes((runner) -> {
             Minecraft.getInstance().player.connection.send(new ServerboundChatCommandPacket("backup snapshot"));
             return 1;
-         }))
+         })
+
+            .then(Commands.argument("name", StringArgumentType.greedyString()).executes((runner) -> {
+                String name = StringArgumentType.getString(runner, "name");
+                Minecraft.getInstance().player.connection.send(new ServerboundChatCommandPacket("backup snapshot " + name));
+                return 1;
+            })))
 
          .then(literal("cancel").executes((runner) -> {
             Minecraft.getInstance().player.connection.send(new ServerboundChatCommandPacket("backup cancel"));
